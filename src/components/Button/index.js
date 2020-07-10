d22ed2b581f5a052c5cc067c385c2df65c36716d
Ryan Brown <ryanbrown@Ryans-MacBook-Pro.local>
2020-07-10 15:45:07 -0500

    component library updates

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import classNames from 'classnames';
+import React, { Component } from 'react';
+import styles from './styles.scss';
+
+export default class Button extends Component {
+    render() {
+        return <div className='btn'>Button</div>;
+    }
+}
